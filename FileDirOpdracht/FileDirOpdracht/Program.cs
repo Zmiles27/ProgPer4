0747081ac0dc42b1b3ec8bbbe2b059aa13c9272a
unknown <34804@ma-web.nl>
2023-06-29 10:09:30 +0200

    oeps hihi

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Gerben\GitProjecten\per4\ProgPer4\ProgPer4\ProgPer4\FileDirOpdracht");
+        PrintFilesAndDirectories(dir);
+    }
+
+    public static void PrintFilesAndDirectories(DirectoryInfo directory)
+    {
+        FileInfo[] files = directory.GetFiles();
+        DirectoryInfo[] Folders = directory.GetDirectories();
+
+        foreach (FileInfo file in files)
+        {
+            Console.WriteLine(file.Name);
+        }
+
+        foreach (DirectoryInfo Folder in Folders)
+        {
+            Console.WriteLine(Folder.FullName);
+            PrintFilesAndDirectories(Folder);
+        }
+    }
+}
+
---
+++ SplitOpdracht/SplitOpdracht/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System;
+using System.Net.Http;
+
+public class Program
+{
+    public static void Main()
+    {
+        string content = File.ReadAllText(@"C:\Users\Gerben\GitProjecten\per4\ProgPer4\ProgPer4\ProgPer4\SplitOpdracht\SplitOpdracht\stringsplit.txt");
+        string[] keyvalue = content.Split(":");
+        foreach(string key in keyvalue)
+        {
+            Console.WriteLine(key);
+        }
+    }
+}
